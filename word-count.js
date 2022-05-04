97c05e327fa3f25cee0b7fae6e0400ba15faec05
Joshyjosh <joshsarnecki@gmail.com>
2022-05-04 13:54:19 -0700

    Initial commit

---
+++ word-count.js
@@ -0,0 +1,13 @@
+
+export const countWords = (str) => {
+  // Split string into array of words
+  let wordArr = str.split(" ");
+  // **Check to see if extra punctuation/white space get their own elements..
+  // Remove punctuation if required
+
+  // Empty result object
+  const result = {};
+  // Loop thru arr, checking against object to see if key already exists .toLowerCase()!!
+  // Add new word key with count value if non-exist, else up count on current word
+
+};
